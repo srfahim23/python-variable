6559440d9b94ec8f2a634c5bca5827c7aa3c6694
Muhammad Faysal <srfahim23@gmail.com>
2023-02-04 22:33:18 +0600

    update-it

--- main.py
+++ main.py
@@ -19,3 +19,22 @@ _color = "blue"    #valid variable name
 NameOfCity = "Mumbai"   #Pascal case
 nameofcity = "Berlin"    #Camel case
 name_of_city = "MOscow"   #snake case
+
+#Scope of variable:
+# icecream = "Vanilla"   #global variable
+# def foods():
+#     vegetable = "Potato"  #local variable
+#     fruti = "Lichi"       #local variable
+#     print(vegetable + " is a local varaible value.")
+#     print(icecream + " is a global variable value.")
+# foods()   
+
+#Global Variable
+icecream = "Vanilla"   #global variable
+def foods():
+    vegetable = "Potato"  #local variable
+    car = "volvo"    #local variable
+    print(vegetable + " is a local variable value.")
+
+foods()
+print(icecream + " is a global varaible value.")    
