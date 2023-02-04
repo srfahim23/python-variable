f27400c286a4669520348b78769a49c05c0779fb
Muhammad Faysal <srfahim23@gmail.com>
2023-02-04 22:16:04 +0600

    update-st

--- main.py
+++ main.py
@@ -4,3 +4,13 @@
 name = "Fahim"   #type str
 age = 20         #type int
 passed = True    #type bool
+
+
+'''
+Variable name can only contain alpha-numeric charecter and underscores(A-z, 0-9, and_)
+Variable name must start with a letter or the underscore charecter.
+Variable are case sensitive.
+Variable name cannot start with a number.
+
+Example
+'''
