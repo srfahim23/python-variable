bd8542ae8ce89753a00f9f9d8e14f08ce81dbcaa
Muhammad Faysal <srfahim23@gmail.com>
2023-02-04 22:19:59 +0600

    update-variable

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # Python-Variable
 
-
+# Example type of variables
 name = "Fahim"   #type str
 age = 20         #type int
 passed = True    #type bool
 
 
-'''
-Variable name can only contain alpha-numeric charecter and underscores(A-z, 0-9, and_)
-Variable name must start with a letter or the underscore charecter.
-Variable are case sensitive.
-Variable name cannot start with a number.
+# Example valid variable 
+Color = "yellow"   #valid variable name
+cOlor = "red"      #valid varaible name
+_color = "blue"    #valid variable name
 
-Example
-'''
+#Example invalid variable
+# 5color = "green"   #invalid variable name
+# $color = "orange"  #invalid variable name
+
